2ffbd724d5819ec716f280baa7cfb1c1f6892d22
Andrew <andrewgrantlind@gmail.com>
2017-10-09 15:48:16 +1300

    Update pi-dartboard.js

--- examples/pi-dartboard.js
+++ examples/pi-dartboard.js
@@ -11,8 +11,8 @@ let inside = (p) => {
  return (x*x + y*y) < 1
 }
 
-parallelize((cpus) => { return _.chain(_.range(NUM_SAMPLES / cpus)).filter(inside).size().value() }, (err, cpus, results) => {
+parallelize((cpus) => { return _.chain(_.range(Math.floor(NUM_SAMPLES / cpus))).filter(inside).size().value() }, (err, cpus, results) => {
  let count = _.reduce(results, (m, i) => { return m + i }, 0)
- console.log(`PI is roughly ${4.0 * (count / (NUM_SAMPLES))}`)
+ console.log(`PI is roughly ${4.0 * (count / NUM_SAMPLES)}`)
  process.exit()
 })
